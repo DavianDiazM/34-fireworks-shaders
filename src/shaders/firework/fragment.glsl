46db8f14c63b557eb9305a5bc0752ef540fa5470
Davian Diaz <daviandiaz5@gmail.com>
2024-09-13 09:33:44 -0400

    First Commit

---
+++ src/shaders/firework/fragment.glsl
@@ -0,0 +1,12 @@
+uniform sampler2D uTexture;
+uniform vec3 uColor;
+
+void main()
+{
+    float textureAlpha = texture(uTexture, gl_PointCoord).r;
+
+    // Final color
+    gl_FragColor = vec4(uColor, textureAlpha);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
---
+++ src/shaders/includes/remap.glsl
@@ -0,0 +1,4 @@
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
